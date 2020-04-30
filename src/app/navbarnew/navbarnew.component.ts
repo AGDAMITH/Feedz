8bce95f08a545d15d9dea046bc2711e0621e13fd
unknown <agdaravinda96@gmail.com>
2020-04-30 10:44:29 +0530

    sec commit

---
+++ src/app/navbarnew/navbarnew.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-navbarnew',
+  templateUrl: './navbarnew.component.html',
+  styleUrls: ['./navbarnew.component.css']
+})
+export class NavbarnewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
